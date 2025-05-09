606b2b31aa7c1477828fe9cee9037bfb68529f99
David Mpongo <mpongodavid723@gmail.com>
2025-05-09 01:55:14 +0100

    Create terminal.js

---
+++ scripts/cmds/terminal.js
@@ -0,0 +1,43 @@
+
+Shell cmd for goatbot
+Use ls < directory > command for listing files
+Use cat < directory > for sending files
+
+const { exec } = require("child_process");
+
+module.exports = {
+  config: {
+    name: "terminal",
+    aliases: ["terminalv2", "$"],
+    author: "Tokodori × Liane", // Convert To Goat By Tokodori
+    role: 2,
+    shortDescription: " ",
+    longDescription: "asking ",
+    category: "ai",
+    guide: "{pn}",
+  },
+
+    onStart : async ({ event, message, args }) => {
+    message.reaction('🚀');
+    exec(args.join(" "), (error, stdout, stderr) => {
+      let result = "";
+      if (error) {
+        result += `Error: ${error.message}
+`;
+      }
+      if (stdout) {
+        result += `${stdout}
+
+`;
+      }
+      if (stderr) {
+        result += `${stderr}
+
+`;
+      }
+
+      message.reply(result);
+      message.reaction("✅");
+    });
+  },
+};
